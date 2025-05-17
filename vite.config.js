53a960eef23a4d2e44825d1096a540477067682d
晨阳 <1900236964@qq.com>
2025-05-17 18:08:44 +0800

    build:引入技术栈tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
     const env = loadEnv(mode, process.cwd())
     return {
-        plugins: [vue()],
+        plugins: [
+            vue(),
+            tailwindcss()
+        ],
         base: env.VITE_BASE_PATH || '/'
     }
 })
